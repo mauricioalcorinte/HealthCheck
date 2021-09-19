4d6f2cefcc8e29f0efa91a3c91e0e7220c99d770
mauricioalcorinte <mauricioalcorinte@gmail.com>
2021-09-19 20:05:40 -0300

    Initial Commit

---
+++ HealthCheck/CustomHealthChecks.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class CustomHealthChecks : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(new HealthCheckResult(
+            status: HealthStatus.Unhealthy,
+            description: "API is sick"
+            ));
+        }
+    }
+}
